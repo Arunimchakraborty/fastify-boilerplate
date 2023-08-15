af979073a44c37cc87d96b667364f741ac4a48da
Arunim Chakraborty <arunimchakraborty@gmail.com>
2023-08-16 00:17:58 +0530

    sme changes

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,47 @@
-import { join } from 'path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
-import { FastifyPluginAsync, FastifyServerOptions } from 'fastify';
+import { join } from "path";
+import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
+import { FastifyPluginAsync, FastifyServerOptions } from "fastify";
 
-export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
-
-}
+export interface AppOptions
+	extends FastifyServerOptions,
+		Partial<AutoloadPluginOptions> {}
 // Pass --options via CLI arguments in command to enable these options.
-const options: AppOptions = {
-}
+const options: AppOptions = {};
 
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+	fastify,
+	opts
 ): Promise<void> => {
-  // Place here your custom code!
-  await fastify.register(AutoLoad, {
+	// Place here your custom code!
+	await fastify.register(AutoLoad, {
 		dir: join(__dirname, "configs"),
 		options: opts,
 	});
-  console.log("Loaded Configs")
+	console.log("Loaded Configs");
 
 	await fastify.register(AutoLoad, {
 		dir: join(__dirname, "services"),
 		options: opts,
 	});
-  console.log("Loaded Services")
+	console.log("Loaded Services");
 
-  // Do not touch the following lines
+	// Do not touch the following lines
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+	// This loads all plugins defined in plugins
+	// those should be support plugins that are reused
+	// through your application
+	void fastify.register(AutoLoad, {
+		dir: join(__dirname, "plugins"),
+		options: opts,
+	});
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts
-  })
-
+	// This loads all plugins defined in routes
+	// define your routes in one of these
+	void fastify.register(AutoLoad, {
+		dir: join(__dirname, "routes"),
+		options: opts,
+	});
 };
 
 export default app;
-export { app, options }
+export { app, options };
